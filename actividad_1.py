acf7125356eb0f907fcff9e1ac5f615c0fb466a5
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-01-27 19:05:02 -0600

    Actividad 1

---
+++ actividad_1.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+folder = "Files"
+
+absolute_path = os.path.abspath(folder)
+
+start_execution_time = time.time()
+
+with open("log.txt", "w") as log_file:
+    start_time = time.time()
+    def open_file(file_name):
+        start_time_file = time.time()
+        with open(os.path.join(folder, file_name), "rb") as html_file:
+            html_file.read()
+        end_time = time.time()
+        read_time = end_time - start_time_file
+
+        log_file.write(f"{os.path.join(absolute_path, file_name)}: {read_time}\n")
+        
+    for file_name in os.listdir(folder):
+        open_file(file_name)
+    total_time = time.time() - start_time
+    total_execution_time = time.time() - start_execution_time
+    log_file.write(f"Tiempo total en abrir los archivos: {total_time}\n")
+    log_file.write(f"Tiempo total de ejecucion: {total_execution_time}\n")
