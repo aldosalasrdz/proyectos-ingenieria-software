d42cce11152565b93e8d465a84052000520b54d3
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-02-03 23:39:22 -0600

    add: bash script to count words

---
+++ count.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Enter the word you want to search:"
+read word
+
+folder="./NewFiles"
+
+if [ ! -d "$folder" ]; then
+  echo "The directory does not exist."
+  exit 1
+fi
+
+total_count=0
+
+for file in "$folder"/*; do
+  count=$(grep -i -o -w "$word" "$file" | wc -l)
+
+  total_count=$((total_count + count))
+
+  if [ $count -gt 0 ]; then
+    echo "$word found $count times in $file"
+  fi
+done
+
+echo "$word found $total_count times in total"
