f477f587f4054ce0d083a9da1bb950f5ecdc97d7
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-03-07 16:30:47 -0600

    add: fase 2, actividad 4

---
+++ fase-2/actividad_4.py
@@ -0,0 +1,44 @@
+"""Actividad 4. Adoptando scrum en el proyecto"""
+import os
+import time
+import re
+
+folder = "NewFiles"
+absolute_path = os.path.abspath(folder)
+
+start_execution_time = time.time()
+
+with open("log4.txt", "w") as log_file:
+    start_time = time.time()
+
+    def save_words(file_name: str) -> list[str]:
+        start_time_read = time.time()
+
+        words: list[str] = []
+
+        with open(os.path.join(folder, file_name), "r") as text_file:
+            content = text_file.read().lower()
+            words += re.findall(r"\b[a-zA-Z]+\b", content)
+        text_file.close()
+
+        end_time_read = time.time()
+        read_time = end_time_read - start_time_read
+        log_file.write(f"{os.path.join(absolute_path, file_name)}: {read_time}\n")
+
+        return words
+
+    all_words: list[str] = []
+    for file_name in os.listdir(folder):
+        all_words += save_words(file_name)
+
+    all_words = sorted(all_words)
+
+    with open("all_words.txt", "w", encoding="utf-8") as all_words_file:
+        all_words_file.write("\n".join(all_words))
+    all_words_file.close()
+
+    total_time = time.time() - start_time
+    total_execution_time = time.time() - start_execution_time
+    log_file.write(f"\nTiempo total en abrir los archivos: {total_time}\n")
+    log_file.write(f"Tiempo total de ejecucion: {total_execution_time}\n")
+log_file.close()
