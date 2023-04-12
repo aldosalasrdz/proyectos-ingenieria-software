9be723987966c4996c77e9064d90acd0a01bb1c7
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-04-12 05:14:20 -0600

    refactor: remove 2 dictionaries

--- fase-3/actividad_7.py
+++ fase-3/actividad_7.py
@@ -15,18 +15,18 @@ with open("log7.txt", "w") as log_file:
     def dictionary_words(file_name: str) -> dict:
         start_time_read = time.time()
 
-        words_dict = {}
+        all_words_dict = {}
 
         with open(os.path.join(folder, file_name), "r") as text_file:
             content = text_file.read().lower()
             words = re.findall(r"\b[a-zA-Z]+\b", content)
             for word in words:
-                if word in words_dict:
-                    words_dict[word]["count"] += 1
-                    if file_name not in words_dict[word]["files"]:
-                        words_dict[word]["files"].append(file_name)
+                if word in all_words_dict:
+                    all_words_dict[word]["count"] += 1
+                    if file_name not in all_words_dict[word]["files"]:
+                        all_words_dict[word]["files"].append(file_name)
                 else:
-                    words_dict[word] = {"count": 1, "files": [file_name]}
+                    all_words_dict[word] = {"count": 1, "files": [file_name]}
         text_file.close()
 
         end_time_read = time.time()
@@ -34,21 +34,13 @@ with open("log7.txt", "w") as log_file:
         log_file.write(f"{os.path.join(absolute_path, file_name)}: {read_time}\n")
 
         with open("posting.txt", "a") as posting_file:
-            for word in words_dict:
-                posting_file.write(f"{file_name};{words_dict[word]['count']}\n")
+            for word in all_words_dict:
+                posting_file.write(f"{file_name};{all_words_dict[word]['count']}\n")
 
-        return words_dict
+        return all_words_dict
 
-    all_words_dict = {}
     for file_name in os.listdir(folder):
-        file_words = dictionary_words(file_name)
-        for word, count in file_words.items():
-            if word in all_words_dict:
-                all_words_dict[word]["count"] += count["count"]
-                if file_name not in all_words_dict[word]["files"]:
-                    all_words_dict[word]["files"].append(file_name)
-            else:
-                all_words_dict[word] = count
+        all_words_dict = dictionary_words(file_name)
 
     with open("dictionary.txt", "w", encoding="utf-8") as dictionary_file:
         word_count = 0
