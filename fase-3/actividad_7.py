303b3de9397e5596e086e51b7d5ce17bde523e40
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-05-04 19:27:22 -0600

    refactor: remove unnecessary comments

--- fase-3/actividad_7.py
+++ fase-3/actividad_7.py
@@ -30,7 +30,8 @@ with open("log7.txt", "w") as log_file:
                     # Se le suma una repeticion
                     # print(words_dict[word])
                     words_dict[word]["RepDic"][file_name] += 1
-                    # Si el archivo no esta en el arreglo dentro del diccionario de la palabra
+                    # Si el archivo no esta en el arreglo dentro del
+                    # diccionario de la palabra
                     if file_name not in words_dict[word]["files"]:
                         # Se agrega
                         words_dict[word]["RepDic"][file_name] = 1
@@ -62,42 +63,26 @@ with open("log7.txt", "w") as log_file:
         # Aqui se le meten las cosas al all_words_dict
         file_words = dictionary_words(file_name)
 
-        # print(file_words)
-        """
-        for row in file_words.items():
-            with open("a.txt", "a") as a:
-                a.write(str(row)+"\n")
-        """
-
         for word, count in file_words.items():
             # Si la palabra esta en el diccionario
             if word in all_words_dict:
                 # El count
-                # words_dict[word][file_name] += 1
                 all_words_dict[word]["fileCount"] += count["count"]
-                # all_words_dict[word][file_name] += 1
 
                 if file_name not in all_words_dict[word]["files"]:
                     all_words_dict[word]["RepDic"][file_name] = count["RepDic"][
                         file_name
                     ]
-                    # all_words_dict[word][file_name] = file_words[word][file_name]
                     all_words_dict[word]["files"].append(file_name)
             else:
                 all_words_dict[word] = count
-                # print(count)
-                # all_words_dict[word][file_name] = file_words[word][file_name]
 
-    # for word in all_words_dict:
-    #   print(str(all_words_dict[word])+"\n")
-    # print()
     with open("posting.txt", "w") as posting_file:
         # Por cada palabra en el diccionario
         for word in all_words_dict:
             # print(all_words_dict[word])
             for file in all_words_dict[word]["files"]:
                 posting_file.write(f"{file};{all_words_dict[word]['RepDic'][file]}\n")
-                # posting_file.write(f"{file};{word};{all_words_dict[word]['150.txt']}\n")
 
     # Se escribe el dictionary
     with open("dictionary.txt", "w", encoding="utf-8") as dictionary_file:
@@ -113,14 +98,3 @@ with open("log7.txt", "w") as log_file:
     log_file.write(f"\nTiempo total en abrir los archivos: {total_time}\n")
     log_file.write(f"Tiempo total de ejecucion: {total_execution_time}\n")
 log_file.close()
-
-# print(words_dict["cdt"])
-"""
-with open("postingPrueba.txt", "a") as posting_file:
-    with open("dictionary.txt", "r") as dic:
-        dicLines = dic.readlines()
-        
-        for line in dicLines
-    for word in words_dict:
-        posting_file.write(f"{file_name};{words_dict[word]['count']}\n")
-"""
