baf0d4499978495e55b283302b26e3fcd4f1f05e
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-05-06 00:18:27 -0600

    fix: rename act9.py to actividad_9.py

--- fase-3/actividad_9.py
+++ fase-3/actividad_9.py
@@ -8,105 +8,52 @@ from hash_table import HashTable
 folder = "NewFiles"
 absolute_path = os.path.abspath(folder)
 
-start_execution_time = time.time()
+Dic = {}
+stopDic = {}
 
-with open("log9.txt", "w") as log_file:
-    start_time = time.time()
+# Se lee el dictionary.txt
+with open("dictionary.txt", "r") as dic:
+    dicLines = dic.readlines()
 
-    hash_table = HashTable(50_000)
+    for line in dicLines:
+        word = line.split(";")[0]
+        rep = line.split(";")[1]
+        pos = line.split(";")[2]
 
-    file_count = {}
+        Dic[word] = {"Rep": rep, "Pos": pos}
 
-    with open("Actividad9_stoplist.pdf", "rb") as stoplist_file:
-        reader = PyPDF2.PdfReader(stoplist_file)
-        stoplist = ""
+    dic.close()
 
-        for page in reader.pages:
-            stoplist += page.extract_text()
+# Se lee el stopList
+with open("Actividad9_stoplist.pdf", "rb") as stoplist_file:
+    reader = PyPDF2.PdfReader(stoplist_file)
+    stoplist = ""
 
-        # Remove everything but the words
-        stoplist = set(re.findall(r"(?!.*TecMilenio).*\b[a-zA-Z]+\b", stoplist))
+    for page in reader.pages:
+        stoplist += page.extract_text()
 
-        # Remove white spaces in each word
-        stoplist = [word.strip() for word in stoplist]
+    # Remove everything but the words
+    stoplist = set(re.findall(r"(?!.*TecMilenio).*\b[a-zA-Z]+\b", stoplist))
 
-        with open("stoplist.txt", "w") as stoplist_file:
-            stoplist_file.write("\n".join(sorted(stoplist)))
+    # Remove white spaces in each word
+    stoplist = [word.strip() for word in stoplist]
 
-    def stop_list_words(file_name: str):
-        start_time_read = time.time()
+    with open("stoplist.txt", "w") as stoplist_file:
+        stoplist_file.write("\n".join(sorted(stoplist)))
+        stoplist_file.close()
 
-        with open(os.path.join(folder, file_name), "r") as text_file:
-            content = text_file.read().lower()
-            words = re.findall(r"\b[a-zA-Z]+\b", content)
+# Se guardan las palabras del stop lost en un dictionary
+with open("stoplist.txt", "r") as stopFile:
+    sT = stopFile.readlines()
 
-            # Remove stoplist words
-            words = [word for word in words if word not in stoplist]
+    for word in sT:
+        stopDic[word[:-1]] = {}
 
-            for word in words:
-                hash_table.add(word, 1)
+    stopFile.close()
 
-                if word not in file_count:
-                    file_count[word] = set()
-
-                file_count[word].add(file_name)
-
-        text_file.close()
-
-        end_time_read = time.time()
-        read_time = end_time_read - start_time_read
-        log_file.write(f"{os.path.join(absolute_path, file_name)}: {read_time}\n")
-
-    for file_name in os.listdir(folder):
-        stop_list_words(file_name)
-
-    # Remove words that appear less than 3 times
-    for i in range(len(hash_table.table)):
-        if hash_table.table[i]:
-            hash_table.table[i] = [
-                (word, count)
-                for word, count in hash_table.table[i]
-                if len(file_count[word]) >= 3
-            ]
-
-    # Remove words of length 1
-    for i in range(len(hash_table.table)):
-        if hash_table.table[i]:
-            hash_table.table[i] = [
-                (word, count) for word, count in hash_table.table[i] if len(word) > 1
-            ]
-
-    MAX_WORD_LENGTH = 26
-    MAX_COUNT_LENGTH = 5
-
-    with open("newHashTable.txt", "w", encoding="utf-8") as hash_table_file:
-        hash_word_count = 0
-        for i in range(len(hash_table.table)):
-            if not hash_table.table[i]:
-                hash_table_file.write("0\n")
-            else:
-                for word, count in hash_table.table[i]:
-                    word_count_str = str(hash_word_count)
-                    count_str = str(len(file_count[word])).ljust(MAX_COUNT_LENGTH + 2)
-                    word_str = str(word).ljust(MAX_WORD_LENGTH + 2)
-                    hash_table_file.write(f"{word_str}{count_str}{word_count_str}\n")
-                    hash_word_count += count
-        hash_table_file.write(
-            f"\nNumero total de colisiones: {hash_table.count_collisions()}"
-        )
-
-    with open("newDictionary.txt", "w", encoding="utf-8") as new_dictionary_file:
-        dict_word_count = 0
-        for i in range(len(hash_table.table)):
-            if hash_table.table[i]:
-                for word, count in hash_table.table[i]:
-                    new_dictionary_file.write(
-                        f"{word};{len(file_count[word])};{dict_word_count}\n"
-                    )
-                    dict_word_count += count
-
-    total_time = time.time() - start_time
-    total_execution_time = time.time() - start_execution_time
-
-    log_file.write(f"\nTiempo total en abrir los archivos: {total_time}\n")
-    log_file.write(f"Tiempo total de ejecucion: {total_execution_time}\n")
+# Si el diccionario stoplist tiene la current palabra o tiene frecuencua <= 3 o es de 1 caracter -> no se escribe en el nuevo diccionario
+with open("newDictionary.txt", "w") as newDic:
+    for word in Dic:
+        if word in stopDic or int(Dic[word]["Rep"]) <= 3 or len(word) == 1:
+            continue
+        newDic.write(f"{word};{Dic[word]['Rep']};{Dic[word]['Pos']}")
