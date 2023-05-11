ba18c3acf2efde65469ed0cab6814c1c157d3fbc
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-05-11 02:13:42 -0600

    feat: add word input prompt for actividad_12 and actividad_13

--- execute.sh
+++ execute.sh
@@ -8,13 +8,29 @@ YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
-for file in $(find fase* -name "*.py" | sort -t_ -k2,2n); do
-    printf "${BLUE}Running file: ${NC}${file}\n"
-    file_start_time=$(date +%s.%N)
-    python3 "$file"
-    file_end_time=$(date +%s.%N)
-    printf "${YELLOW}The file ${file} took $(echo "$file_end_time - $file_start_time" | bc) seconds to execute.${NC}\n\n"
+for dir in fase-{1..4}; do
+  for file in $(find $dir -name "*.py" | sort -t_ -k2,2n); do
+      printf "${BLUE}Running file: ${NC}${file}\n"
+
+      if [[ "$file" == "fase-4/actividad_12.py" || "$file" == "fase-4/actividad_13.py" ]]; then
+        read -p "Enter a word input: " word_input
+        file_start_time=$(date +%s.%N)
+
+        python3 "$file" "$word_input"
+
+        file_end_time=$(date +%s.%N)
+        printf "${YELLOW}The file ${file} took %.2f seconds to execute.${NC}\n\n" $(echo "$file_end_time - $file_start_time" | bc)
+      else
+        file_start_time=$(date +%s.%N)
+
+        python3 "$file"
+
+        file_end_time=$(date +%s.%N)
+        printf "${YELLOW}The file ${file} took %.2f seconds to execute.${NC}\n\n" $(echo "$file_end_time - $file_start_time" | bc)
+      fi
+  done
 done
 
 end_time=$(date +%s.%N)
-printf "${CYAN}The script took $(echo "$end_time - $start_time" | bc) seconds to execute.${NC}\n"
+
+printf "${CYAN}The script took %.2f seconds to execute.${NC}\n" $(echo "$end_time - $start_time" | bc)
