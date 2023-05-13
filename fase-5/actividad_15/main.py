03f2ba47f8e94ea75b7fb83cb930b1cedc7ef6d7
Aldo Salas <52344459+aldosalasrdz@users.noreply.github.com>
2023-05-13 01:34:46 -0600

    feat: add actividad_15

---
+++ fase-5/actividad_15/main.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, send_file
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def search():
+    word = request.args.get("word", "").lower()
+
+    if os.path.basename(os.getcwd()) == "actividad_15":
+        root_directory = os.path.abspath("../..")
+    else:
+        root_directory = os.path.abspath("./")
+
+    # Se lee el diccionario
+    with open(os.path.join(root_directory, "dictionary.txt"), "r") as Dic:
+        DicLines = Dic.readlines()
+        Dic.close()
+
+    # Se lee el posting
+    with open(os.path.join(root_directory, "newPosting.txt"), "r") as Pos:
+        PosLines = Pos.readlines()
+        Pos.close()
+
+    # Se lee el documents
+    with open(os.path.join(root_directory, "Documents.txt"), "r") as Doc:
+        DocLines = Doc.readlines()
+        Doc.close()
+
+    files = []
+    for line in DicLines:
+        rowWord = line.split(";")[0]
+        if word == rowWord:
+            rowRep = int(line.split(";")[1])
+            rowPos = int(line.split(";")[2])
+
+            for i in range(rowPos, (rowPos + rowRep)):
+                fileId = int(PosLines[i].split()[0]) - 1
+                file = DocLines[fileId].split()[1]
+
+                file_path = os.path.join("templates", file)
+
+                files.append((file, file_path))
+
+            break
+
+    return render_template("index.html", word=word, files=files)
+
+
+@app.route("/files/<path:filename>")
+def view_file(filename):
+    return send_file(filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
